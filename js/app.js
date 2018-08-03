14fa5133b26f315afc1e29cb558994cff7f186d8
Karina Pereira <karina.biologa@gmail.com>
2018-08-03 14:09:46 -0300

    Splashscreen com transição funcionando bem, mapa aparecendo com a localização, falta o marker.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
-// Add your JavaScript
+
+$(document).ready(function() {
+  $('.home').hide();
+  $(document).ready(function() {
+    $('.splashscreen').delay('2500').fadeOut('slow')
+    $('.home').delay('2500').fadeIn('slow')
+  });
+  
+
+  var map;
+ 
+  function initialize() {
+    var myLocation = new google.maps.LatLng(-23.5578203, -46.6644501);
+     
+    var options = {
+      zoom: 16,
+      center: myLocation,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var marker = new google.maps.Marker({
+      position: myLocation,
+      title: "Você Está aqui!",
+      map: map,
+  });
+     
+    map = new google.maps.Map(document.getElementById("map"), options, marker);
+    }
+     
+    initialize();
+  })
+
+  
